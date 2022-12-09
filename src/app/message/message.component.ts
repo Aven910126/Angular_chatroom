286b91057522efca4777c397cb1b71797c196a5e
fan9704 <cxz123499@gmail.com>
2022-12-09 22:48:10 +0800

    Redesign Message Component

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FriendService } from '../friend.service';
+import {BehaviorSubject, Observable, switchMap} from 'rxjs';
+import {child, Database, get, getDatabase} from "@angular/fire/database";
+import {AngularFireAction, AngularFireDatabase} from "@angular/fire/compat/database";
+import {DataSnapshot} from "@angular/fire/compat/database/interfaces";
+import firebase from "firebase/compat";
+
 @Component({
   selector: 'app-message',
   templateUrl: './message.component.html',
@@ -8,11 +14,27 @@ import { FriendService } from '../friend.service';
 export class MessageComponent implements OnInit {
   img;
   name = "local"
-  constructor(friendService : FriendService) { 
+  messageList:Array<any>=[];
+  private messagesRef:any;
+  private db:AngularFireDatabase
+  message: any;
+  constructor(friendService : FriendService,db: AngularFireDatabase) {
       this.img = friendService.getimg();
+      this.db =db;
+      db.list('message').valueChanges().subscribe(res=>{
+        console.log("Receive New Msg"+res[res.length-1])
+        this.messageList.push(res[res.length-1]);
+      });
+      this.messagesRef = db.object('message');
   }
 
   ngOnInit(): void {
+    // @ts-ignore
+    this.db.list('message').valueChanges().subscribe(res=>{
+      this.messageList=res;
+      console.log(this.messageList)
+    });
   }
+
 
 }
