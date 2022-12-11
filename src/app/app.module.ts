4e7879b643f8a00bc07876181e60b488cd831581
fan9704 <cxz123499@gmail.com>
2022-12-11 17:15:51 +0800

    Complete Firebase OAuth Google Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,9 @@ import { FriendService } from './friend.service';
 import { RoomComponent } from './room/room.component';
 import { MessageComponent } from './message/message.component';
 import { BarComponent } from './bar/bar.component';
-import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { USE_DEVICE_LANGUAGE } from '@angular/fire/compat/auth';
 import {firebaseConfig} from "../firebase/firebaseUtil"
-import {initializeApp, getApp, provideFirebaseApp} from "@angular/fire/app";
+import { SETTINGS as AUTH_SETTINGS } from '@angular/fire/compat/auth';
 import {AngularFireDatabase, AngularFireDatabaseModule} from "@angular/fire/compat/database";
 import {AngularFireModule} from "@angular/fire/compat";
 import {FormsModule} from "@angular/forms";
@@ -32,7 +32,12 @@ import {FormsModule} from "@angular/forms";
         AngularFireDatabaseModule,
         FormsModule,
     ],
-  providers: [FriendService,AngularFireDatabase],
+  providers: [
+    FriendService,
+    AngularFireDatabase,
+    { provide: AUTH_SETTINGS, useValue: { appVerificationDisabledForTesting: true } },
+    // { provide: USE_DEVICE_LANGUAGE, useValue: true },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
