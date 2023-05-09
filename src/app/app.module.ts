6e2ee039c2e4135622511cc9dfb53308b0cd795a
Aven910126 <a1148370488@gmail.com>
2023-05-09 23:23:59 +0800

    success img upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,15 +12,17 @@ import { RoomComponent } from './room/room.component';
 import { MessageComponent } from './message/message.component';
 import { BarComponent } from './bar/bar.component';
 import { USE_DEVICE_LANGUAGE } from '@angular/fire/compat/auth';
-import {firebaseConfig} from "../firebase/firebaseUtil"
+import { firebaseConfig } from "../firebase/firebaseUtil"
 import { SETTINGS as AUTH_SETTINGS } from '@angular/fire/compat/auth';
-import {AngularFireDatabase, AngularFireDatabaseModule} from "@angular/fire/compat/database";
-import {AngularFireModule} from "@angular/fire/compat";
-import {FormsModule} from "@angular/forms";
+import { AngularFireDatabase, AngularFireDatabaseModule } from "@angular/fire/compat/database";
+import { AngularFireModule } from "@angular/fire/compat";
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { FormsModule } from "@angular/forms";
 import { LoginComponent } from './login/login.component';
-import {ServiceWorkerModule} from "@angular/service-worker";
+import { ServiceWorkerModule } from "@angular/service-worker";
 import { OverlayModule } from "@angular/cdk/overlay";
-import {AngularFireMessagingModule} from "@angular/fire/compat/messaging";
+import { AngularFireMessagingModule } from "@angular/fire/compat/messaging";
+import { FileUploadService } from './file-upload.service';
 @NgModule({
   declarations: [
     AppComponent,
@@ -31,25 +33,27 @@ import {AngularFireMessagingModule} from "@angular/fire/compat/messaging";
     BarComponent,
     LoginComponent,
   ],
-    imports: [
-        BrowserModule,
-        FormsModule,
-        AppRoutingModule,
-        AngularFireModule.initializeApp(firebaseConfig),
-        AngularFireDatabaseModule,
-        OverlayModule,
-        AngularFireMessagingModule,
-        ServiceWorkerModule.register('ngsw-worker.js', {
-          enabled: true,
-          // Register the ServiceWorker as soon as the application is stable
-          // or after 30 seconds (whichever comes first).
-          registrationStrategy: 'registerWhenStable:30000'
-        }),
-    ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    OverlayModule,
+    AngularFireMessagingModule,
+    AngularFireStorageModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: true,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
+  ],
   providers: [
     FriendService,
     EditService,
-    ChangeroomService,
+    ChangeroomService, 
+    FileUploadService,
     AngularFireDatabase,
     { provide: AUTH_SETTINGS, useValue: { appVerificationDisabledForTesting: true } },
     // { provide: USE_DEVICE_LANGUAGE, useValue: true },
